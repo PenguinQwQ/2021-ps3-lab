983bcda3266e615385fe511ba4dd6ebcdd2188dc
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-09-10 21:47:52 +0800

    compile

--- include/DataStructures/Graph.h
+++ include/DataStructures/Graph.h
@@ -2,6 +2,8 @@
 #define GRAPH_H
 
 #include <vector>
+#include <map>
+
 #include <DataStructures/Edge.h>
 
 class Graph {
@@ -24,7 +26,7 @@ class Graph {
   std::vector<Edge> GetOutgoingEdges(int vertex) const;
   int GetDegree(int vertex) const;
   std::vector<int> GetNeighbors(int vertex) const;
-  private:
+  public:
    std::map<int, std::vector<int> > Edges;//构造一个map，map[x]返回x键值节点的相连边的列表，用来存储边
    std::vector<int> vertexs; //构造一个vector，用来存储点。
 };
