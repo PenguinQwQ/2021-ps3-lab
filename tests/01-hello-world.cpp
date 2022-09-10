85a5a41433cccc46a69359717a43a309e1fdbc26
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-09-10 23:17:07 +0800

    compile

--- tests/01-hello-world.cpp
+++ tests/01-hello-world.cpp
@@ -10,19 +10,12 @@ Graph g; // 创建一张新的图
 assert(g.AddVertex(1) == true);
 assert(g.AddVertex(2) == true);
 assert(g.AddVertex(3) == true);
-assert(g.AddVertex(3) == false); // 3号点已经存在了
-assert(g.AddVertex(4) == true);
-assert(g.ContainsVertex(4) == true);
-assert(g.RemoveVertex(4) == true);
-assert(g.ContainsVertex(4) == false);
-assert(g.RemoveVertex(5) == false);
-
 assert(g.AddEdge(1, 2) == true);
-assert(g.AddEdge(1, 3) == true);
-assert(g.AddEdge(2, 5) == false); // 5号点不存在
-assert(g.ContainsEdge(1, 2) == true);
-assert(g.ContainsEdge(2, 5) == false);
-
+assert(g.AddEdge(1, 1) == true);
+assert(g.AddEdge(1, 1) == false);
+assert(g.ContainsEdge(1, 1) == true);
+assert(g.RemoveEdge(1,1) == true);
+assert(g.AddEdge(1, 1) == true);
 assert(g.GetVertices().size() == g.CountVertices()); // 4
 /*
 printf("%lu\n", g.GetEdges().size());
@@ -33,7 +26,7 @@ for(std::vector<Edge>::iterator it = test.begin() ; it != test.end() ; it++)
     }
 */
 assert(g.GetEdges().size() == 2);
-assert(g.GetIncomingEdges(1).size() == 0);
+assert(g.GetIncomingEdges(1).size() == 1);
 assert(g.GetOutgoingEdges(1).size() == 2); // {{1, 2}, {1, 3}}
 assert(g.GetDegree(1) == 2);
 assert(g.GetNeighbors(1).size() == 2); // {2, 3}
