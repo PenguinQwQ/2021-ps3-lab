17b6420eb9c9bf65aff86e98dcd4cf8d2682c1a7
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-09-24 00:17:39 +0800

    compile

--- tests/01-hello-world.cpp
+++ tests/01-hello-world.cpp
@@ -4,64 +4,35 @@
 #include <cstdio>
 #include <DataStructures/WeightedEdge.h>
 #include <DataStructures/WeightedGraph.h>
+#include <DataStructures/UndirectedGraph.h>
+#include <DataStructures/UndirectedWeightedGraph.h>
 int main()
 {
-WeightedGraph g; // 创建一张新的图
-
-/*
-assert(g.AddVertex(1) == true);
-assert(g.AddVertex(2) == true);
-assert(g.AddVertex(3) == true);
-assert(g.AddVertex(3) == false); // 3号点已经存在了
-assert(g.AddVertex(4) == true);
-assert(g.ContainsVertex(4) == true);
-assert(g.RemoveVertex(4) == true);
-assert(g.ContainsVertex(4) == false);
-assert(g.RemoveVertex(5) == false);
-
-assert(g.AddEdge(1, 2, 1) == true);
-assert(g.AddEdge(1, 3, 2) == true);
-assert(g.AddEdge(2, 5, 3) == false); // 5号点不存在
-assert(g.ContainsEdge(1, 2) == true);
-assert(g.ContainsEdge(2, 5) == false);
-
-assert(g.GetVertices().size() == g.CountVertices()); // 4
-assert(g.GetEdges().size() == 2);
-assert(g.GetIncomingEdges(1).size() == 0);
-assert(g.GetOutgoingEdges(1).size() == 2); // {{1, 2}, {1, 3}}
-assert(g.GetDegree(1) == 2);
-assert(g.GetNeighbors(1).size() == 2); // {2, 3}
-assert(g.GetNeighbors(2).size() == 0);
-
-*/
-
+UndirectedGraph g;
+//
 assert(g.AddVertex(1) == true);
 assert(g.AddVertex(2) == true);
 assert(g.AddVertex(3) == true);
 assert(g.AddVertex(4) == true);
 assert(g.AddVertex(5) == true);
-assert(g.AddVertex(1) == false);
-assert(g.AddVertex(2) == false);
-assert(g.AddVertex(3) == false);
-assert(g.AddVertex(4) == false);
-assert(g.AddVertex(5) == false);
 
 assert(g.RemoveVertex(5) == true);
 assert(g.RemoveVertex(5) == false);
 
-assert(g.AddEdge(1, 2, 1) == true);
-assert(g.AddEdge(1, 2, 2) == false);
-assert(g.AddEdge(1, 3, 3) == true);
-assert(g.AddEdge(2, 1, 4) == true);
-assert(g.AddEdge(1, 1, 5) == true);
-assert(g.AddEdge(1, 1, 2) == false);
-assert(g.ContainsEdge(1,1) == true);
+assert(g.AddEdge(1, 2) == true);
+assert(g.AddEdge(1, 2) == false);
+assert(g.AddEdge(2, 1) == false);
+
+assert(g.AddEdge(1, 3) == true);
+assert(g.AddEdge(1, 1) == true);
+assert(g.AddEdge(1, 1) == false);
+assert(g.ContainsEdge(1, 1) == true);
 //1->2, 1->3, 2->1, 1->1
 
 assert(g.RemoveEdge(1,1) == true);
 assert(g.ContainsEdge(1,1) == false);
 assert(g.RemoveEdge(1,1) == false);
-assert(g.AddEdge(1, 1, 5) == true);
+assert(g.AddEdge(1, 1) == true);
 
 assert(g.CountVertices() == 4);
 assert(g.CountEdges() == 4);
@@ -71,13 +42,11 @@ assert(g.ContainsVertex(1) == true);
 assert(g.ContainsEdge(1,1) == true);
 assert(g.ContainsEdge(1,2) == true);
 assert(g.ContainsEdge(2,1) == true);
-assert(g.ContainsEdge(3,1) == false);
-assert(g.GetWeight(1,1) == 5);
+assert(g.ContainsEdge(3,1) == true);
 
 assert(g.GetEdges().size() == 4);
 assert(g.GetDegree(1) == 3);
 assert(g.GetNeighbors(1).size() == 3);
-
 /*
 printf("%lu\n", g.GetEdges().size());
 std::vector<Edge> test = g.GetEdges();
