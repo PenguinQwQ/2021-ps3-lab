98e3e2d7f78efcfbcfff31d57fe243c1da5fc09d
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-10-01 11:45:54 +0800

    compile

--- tests/01-hello-world.cpp
+++ tests/01-hello-world.cpp
@@ -8,5 +8,14 @@
 #include <DataStructures/UndirectedWeightedGraph.h>
 int main()
 {
+    WeightedGraph<double> g;
+    assert(g.AddVertex(1) == true);
+    assert(g.AddVertex(2) == true);
+    assert(g.AddVertex(3) == true);
+    assert(g.AddVertex(1) == false);  
+    assert(g.AddEdge(1,2,0.2) == true);
+    assert(g.AddEdge(1,3,0.3) == true);
+    assert(g.GetWeight(1, 2) == 0.2);
+    
     return 0;
 }
