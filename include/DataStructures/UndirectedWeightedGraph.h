37906c81696e359b5f769c91ae5a973c88c69e30
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-10-01 11:58:21 +0800

    compile

--- include/DataStructures/UndirectedWeightedGraph.h
+++ include/DataStructures/UndirectedWeightedGraph.h
@@ -60,7 +60,7 @@ bool UndirectedWeightedGraph<T2>::RemoveEdge(int vertex1, int vertex2)
     if(vertex1 == vertex2)
     {
         WeightedGraph<T2>::RemoveEdge(vertex1, vertex2);
-        this->selfring.erase(std::find(this->selfring.begin(), this->selfring.end(), vertex1));
+        this->selfring.erase(algorithm::find(this->selfring.begin(), this->selfring.end(), vertex1));
         return true;
     }
     //Third remove the remaining contents
