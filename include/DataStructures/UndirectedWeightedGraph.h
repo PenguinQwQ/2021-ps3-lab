b45deed9c1944b64f6ad7a748054ee2f2da2cfcd
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-10-01 11:49:32 +0800

    compile

--- include/DataStructures/UndirectedWeightedGraph.h
+++ include/DataStructures/UndirectedWeightedGraph.h
@@ -6,7 +6,7 @@
 #include <DataStructures/WeightedGraph.h>
 #include <utility>
 #include <vector>
-template <class T1, class T2>
+template <class T2>
 class UndirectedWeightedGraph : public WeightedGraph<T2> {
     public:
     UndirectedWeightedGraph();
@@ -20,18 +20,18 @@ class UndirectedWeightedGraph : public WeightedGraph<T2> {
     std::vector<int> selfring;
 };
 
-template <class T1, class T2>
-UndirectedWeightedGraph<T1,T2>::UndirectedWeightedGraph()
+template <class T2>
+UndirectedWeightedGraph<T2>::UndirectedWeightedGraph()
 {
     selfring.clear();
 }
 
-template <class T1, class T2>
-UndirectedWeightedGraph<T1,T2>::~UndirectedWeightedGraph()
+template <class T2>
+UndirectedWeightedGraph<T2>::~UndirectedWeightedGraph()
 {}
 
-template <class T1, class T2>
-bool UndirectedWeightedGraph<T1,T2>::AddEdge(int vertex1, int vertex2, T2 weight)
+template <class T2>
+bool UndirectedWeightedGraph<T2>::AddEdge(int vertex1, int vertex2, T2 weight)
 {
     //First check the vertexs are in the graph
     if(WeightedGraph<T2>::ContainsVertex(vertex1) == false)
@@ -44,14 +44,14 @@ bool UndirectedWeightedGraph<T1,T2>::AddEdge(int vertex1, int vertex2, T2 weight
     if(vertex1 == vertex2)
     {
         WeightedGraph<T2>::AddEdge(vertex1, vertex2, weight);
-        UndirectedWeightedGraph<T1,T2>::selfring.push_back(vertex1);
+        UndirectedWeightedGraph<T2>::selfring.push_back(vertex1);
         return true;
     }
     //Third solve the remaining situation
     return WeightedGraph<T2>::AddEdge(vertex1, vertex2, weight) && WeightedGraph<T2>::AddEdge(vertex2, vertex1, weight);
 }
-template <class T1, class T2>
-bool UndirectedWeightedGraph<T1, T2>::RemoveEdge(int vertex1, int vertex2)
+template <class T2>
+bool UndirectedWeightedGraph<T2>::RemoveEdge(int vertex1, int vertex2)
 {
     //First, check the vertexs exists and the Edge exists
     if(WeightedGraph<T2>::ContainsEdge(vertex1, vertex2) == false)
@@ -60,20 +60,20 @@ bool UndirectedWeightedGraph<T1, T2>::RemoveEdge(int vertex1, int vertex2)
     if(vertex1 == vertex2)
     {
         WeightedGraph<T2>::RemoveEdge(vertex1, vertex2);
-        this->selfring.erase(UndirectedWeightedGraph<T1, T2>::find(this->selfring.begin(), this->selfring.end(), vertex1));
+        this->selfring.erase(UndirectedWeightedGraph<T2>::find(this->selfring.begin(), this->selfring.end(), vertex1));
         return true;
     }
     //Third remove the remaining contents
     return WeightedGraph<T2>::RemoveEdge(vertex1, vertex2) && WeightedGraph<T2>::RemoveEdge(vertex2, vertex1);
 }
-template <class T1, class T2>
-int UndirectedWeightedGraph<T1, T2>::CountEdges() const
+template <class T2>
+int UndirectedWeightedGraph<T2>::CountEdges() const
 {
     int cnt = WeightedGraph<T2>::CountEdges() + selfring.size();
     return cnt >> 1;
 }
-template <class T1, class T2>
-std::vector<WeightedEdge<T2>> UndirectedWeightedGraph<T1,T2>::GetEdges() const
+template <class T2>
+std::vector<WeightedEdge<T2>> UndirectedWeightedGraph<T2>::GetEdges() const
 {
     std::vector<WeightedEdge<T2>> t;//ans set
     t.clear();
@@ -88,13 +88,13 @@ std::vector<WeightedEdge<T2>> UndirectedWeightedGraph<T1,T2>::GetEdges() const
     }
     return t;
 }
-template <class T1, class T2>
-int UndirectedWeightedGraph<T1, T2>::GetDegree(int vertex) const
+template <class T2>
+int UndirectedWeightedGraph<T2>::GetDegree(int vertex) const
 {
     if(WeightedGraph<T2>::ContainsVertex(vertex) == false)
         return 0;
     int cnt = WeightedGraph<T2>::GetDegree(vertex);
-    if(UndirectedWeightedGraph<T1, T2>::find(this->selfring.begin(), this->selfring.end(), vertex) != this->selfring.end())
+    if(UndirectedWeightedGraph<T2>::find(this->selfring.begin(), this->selfring.end(), vertex) != this->selfring.end())
         cnt++;
     return cnt;
 }
