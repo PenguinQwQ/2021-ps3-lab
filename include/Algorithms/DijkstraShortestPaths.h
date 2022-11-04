666fa988e4466bb526ba1d742fc7059b6283605a
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-04 15:56:49 +0800

    compile

--- include/Algorithms/DijkstraShortestPaths.h
+++ include/Algorithms/DijkstraShortestPaths.h
@@ -9,13 +9,8 @@
 template <template<typename> class TGraph, typename TValue>
 class DijkstraShortestPaths : public ShortestPaths<TGraph, TValue>{
  public:
-  DijkstraShortestPaths(const TGraph<TValue> *graph, int source);
-  ~DijkstraShortestPaths(){};
-};
-
-template <template<typename> class TGraph, typename TValue>
-DijkstraShortestPaths<TGraph, TValue>::DijkstraShortestPaths(const TGraph<TValue> *graph, int source)
-{
+  DijkstraShortestPaths(const TGraph<TValue> *graph, int source)
+  {
     this->INF = TValue();
     for (auto it : graph->GetEdges())
     {
@@ -50,4 +45,45 @@ DijkstraShortestPaths<TGraph, TValue>::DijkstraShortestPaths(const TGraph<TValue
     }
 }
 
+  ~DijkstraShortestPaths(){};
+};
+/*
+template <template<typename> class TGraph, typename TValue>
+DijkstraShortestPaths<TGraph, TValue>::DijkstraShortestPaths(const TGraph<TValue> *graph, int source)
+
+{
+    this->INF = TValue();
+    for (auto it : graph->GetEdges())
+    {
+        this->INF = this->INF + it.GetWeight();
+    }
+
+    for (auto it : graph->GetVertices())
+    {
+        this->d[it] = this->INF;
+        this->vis[it] = false;
+        this->prev[it] = 0;
+    }
+    this->d[source] = TValue();
+    this->pq.push(std::make_pair(this->d[source], source));
+    while (this->pq.size() > 0)
+    {
+        int u = this->pq.top().second;
+        TValue dis = this->pq.top().first;
+        if(this->vis[u]) continue;
+        this->vis[u] = true;
+        for (auto it : graph->GetOutgoingEdges(u))
+        {
+            TValue w = it.GetWeight();
+            int v = it.GetDestination();
+            if(this->d[v] > dis + w)
+            {
+                this->d[v] = dis + w;
+                this->pq.push(std::make_pair(this->d[v], v));
+                this->prev[v] = u;
+            }
+        }
+    }
+}
+*/
 #endif
