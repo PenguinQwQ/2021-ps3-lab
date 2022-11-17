1e071c60fffcfd85c51538aff9241a8676d6fd85
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-17 21:40:00 +0800

    compile

--- include/Algorithms/ShortestPaths.h
+++ include/Algorithms/ShortestPaths.h
@@ -13,19 +13,19 @@ class ShortestPaths {
   ~ShortestPaths(){};
  public:
   bool HasPathTo(int destination) const;
-  std::optional<TGraph::value_type> TryGetDistanceTo(int destination) const;
+  std::optional<typename TGraph::value_type> TryGetDistanceTo(int destination) const;
   std::optional<std::vector<int>> TryGetShortestPathTo(int destination) const;
  public:
   bool vis[N];//reachable point set 
   int prev[N]; 
-  TGraph::value_type INF;
-  TGraph::value_type d[N];//this is the array of dis!
-  std::priority_queue <std::pair<TGraph::value_type, int>, std::vector<std::pair<TGraph::value_type, int>> , std::greater<std::pair<TGraph::value_type, int>> > pq;
+  typename TGraph::value_type INF;
+  typename TGraph::value_type d[N];//this is the array of dis!
+  std::priority_queue <std::pair<typename TGraph::value_type, int>, std::vector<std::pair<typename TGraph::value_type, int>> , std::greater<std::pair<typename TGraph::value_type, int>> > pq;
 };
 
 
 template <typename TGraph>
-std::optional<TGraph::value_type> ShortestPaths<TGraph, TGraph::value_type>::TryGetDistanceTo(int destination) const
+std::optional<typename TGraph::value_type> ShortestPaths<TGraph, typename TGraph::value_type>::TryGetDistanceTo(int destination) const
 {
   if(vis[destination]) return d[destination];
   else return std::nullopt;
