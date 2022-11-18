a54bed45705f3d847f5c3a1fc58e598f1a49adce
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-18 17:34:25 +0800

    compile

--- include/Algorithms/BellmanFordShortestPaths.h
+++ include/Algorithms/BellmanFordShortestPaths.h
@@ -56,11 +56,10 @@ BellmanFordShortestPaths<TGraph>::BellmanFordShortestPaths(const TGraph *graph, 
     this->d[source] = typename TGraph::value_type();
     this->vis[source] = true;
     int V = vertices.size();
-
-        for (auto u : vertices)
-        for (auto e : graph->OutEdges[u])
+    for (int i = 1 ; i <= V - 1 ; i++)
+        for (auto e : edges)
             {
-                int v = e.GetDestination();
+                int u = e.GetSource(), v = e.GetDestination();
                 typename TGraph::value_type w = e.GetWeight();
                 printf("%d->%d with weight %d \n", u, v, w);
             if(this->vis[u] == false) continue;
