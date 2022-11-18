55a742e0b79c0c72d4e730a5983a8e7e2da179c0
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-18 11:32:19 +0800

    compile

--- include/DataStructures/WeightedGraph.h
+++ include/DataStructures/WeightedGraph.h
@@ -37,7 +37,7 @@ template <class T2>
 WeightedGraph<T2>::WeightedGraph():Graph()
 { 
     static_assert(std::is_constructible<T2>::value == true, "TValue requires default constructor");
-    static_assert(std::is_arithmetic<T2>::value == true, "TValue requires operator+");
+ //   static_assert(std::is_arithmetic<T2>::value == true, "TValue requires operator+");
     this->G.clear();}
 //Destructor
 template <class T2>
