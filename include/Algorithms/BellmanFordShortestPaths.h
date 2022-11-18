555de590523c2715b0b3bc44d3e1fddf2cfea08f
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-18 17:44:23 +0800

    compile

--- include/Algorithms/BellmanFordShortestPaths.h
+++ include/Algorithms/BellmanFordShortestPaths.h
@@ -32,7 +32,7 @@ BellmanFordShortestPaths<TGraph>::BellmanFordShortestPaths(const TGraph *graph, 
     this->reach[source] = true;
     this->dfn[source] = (this->cnt)++;
     int V = vertices.size();
-    for (int i = 1 ; i <= V - 1 ; i++)
+    for (int i = 1 ; i <= V * 2 ; i++)
         for (auto e : edges)
             {
                 int u = e.GetSource(), v = e.GetDestination();
