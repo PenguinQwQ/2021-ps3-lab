f6faafada118f7b07ddd4fceabe2438ab75e8f18
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-18 17:55:24 +0800

    compile

--- include/Algorithms/BellmanFordShortestPaths.h
+++ include/Algorithms/BellmanFordShortestPaths.h
@@ -35,24 +35,53 @@ BellmanFordShortestPaths<TGraph>::BellmanFordShortestPaths(const TGraph *graph, 
     for (int i = 1 ; i <= V - 1 ; i++)
         for (auto e : edges)
             {
-                int u = e.GetSource(), v = e.GetDestination();
+                int u = e.GetSource(), v = e.GetDestination(), undir_u = 0, undir_v = 0;
                 typename TGraph::value_type w = e.GetWeight();
                 printf("%d->%d with weight %d \n", u, v, w);
-            if(this->vis[u] == false) continue;
+            if(graph->G.find(pair<int, int>(v, u)) != graph->G.end())        
+                {
+                    undir_u = v; undir_v = u;
+                }
+            while(1)
+            {
+            if(this->vis[u] == false) break;;
             if(this->vis[u] && (this->vis[v] == false))
                 {
                     this->dfn[v] = (this->cnt)++;
                     this->vis[v] = true;
                     this->d[v] = this->d[u] + w;
                     this->prev[v] = u;
-                    continue;
+                    break;
                 }
             if(this->vis[u] && this->vis[v] && (this->dfn[u] < this->dfn[v]) && (this->d[v] > this->d[u] + w))
             {
                 this->d[v] = this->d[u] + w;
                 this->prev[v] = u;
+                break;
+            }
+            break;
+            }
+            if(undir_u && undir_v)
+            {
+                u = undir_u;
+                v = undir_v;
+            if(this->vis[v] == false) break;;
+            if(this->vis[v] && (this->vis[u] == false))
+                {
+                    this->dfn[u] = (this->cnt)++;
+                    this->vis[u] = true;
+                    this->d[u] = this->d[v] + w;
+                    this->prev[u] = v;
+                    continue;
+                }
+            if(this->vis[u] && this->vis[v] && (this->dfn[v] < this->dfn[u]) && (this->d[u] > this->d[v] + w))
+            {
+                this->d[u] = this->d[v] + w;
+                this->prev[u] = v;
                 continue;
             }
+            }
+
         }
 
  /*   for (auto p : vertices)
